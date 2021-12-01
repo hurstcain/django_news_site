aad5c045388729b4f8357749db7ada48a462b5dd
Ksenia Martynova <hurstcain@gmail.com>
2021-12-01 21:31:45 +0300

    Added pagination

--- news/views.py
+++ news/views.py
@@ -1,22 +1,25 @@
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from news.models import News, Category
 from news.forms import NewsForm
 
 
 class HomeNews(ListView):
-    queryset = News.objects.filter(is_published=True)
+    queryset = News.objects.filter(is_published=True).select_related('category')
     template_name = 'news/index.html'
     context_object_name = 'news'
+    paginate_by = 10
 
 
 class NewsCategory(ListView):
     template_name = 'news/category.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 10
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True, category_id=self.kwargs['category_id'])
+        return News.objects.filter(is_published=True, category_id=self.kwargs['category_id']).select_related('category')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -26,12 +29,13 @@ class NewsCategory(ListView):
 
 
 class ViewNews(DetailView):
-    queryset = News.objects.filter(is_published=True)
+    queryset = News.objects.filter(is_published=True).select_related('category')
     pk_url_kwarg = 'news_id'
     template_name = 'news/view_news.html'
     context_object_name = 'news'
 
 
-class AddNews(CreateView):
+class AddNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
+    login_url = '/admin/'
